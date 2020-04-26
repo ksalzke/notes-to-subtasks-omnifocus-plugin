7a4e4a00cb2f5a39d68ab8432b80d5adc7d78b38
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-04-26 15:57:13 +1000

    Don't expand if no TaskPaper/note

--- Resources/noteToSubtasksLib.js
+++ Resources/noteToSubtasksLib.js
@@ -36,57 +36,59 @@
     // replace '[ ]' with '-'
     taskpaper = taskpaper.replace(/\[\s\]/g, " - ");
 
-    //create list of tags from original task
-    var tagArray = [];
-    task.tags.forEach(function (tag) {
-      if (!uninheritedTags.includes(tag)) {
-        tagArray.push(tag.name);
-      }
-    });
-    tagList = tagArray.join(", ");
+    if (taskpaper !== "") {
+      //create list of tags from original task
+      var tagArray = [];
+      task.tags.forEach(function (tag) {
+        if (!uninheritedTags.includes(tag)) {
+          tagArray.push(tag.name);
+        }
+      });
+      tagList = tagArray.join(", ");
 
-    // add parent & checklist tags (where there are existing tags on line in taskpaper)
-    taskpaper = taskpaper.replace(
-      /@tags\((.+)\)/gm,
-      `@tags($1, ${checklistTagName}, ${tagList})`
-    );
+      // add parent & checklist tags (where there are existing tags on line in taskpaper)
+      taskpaper = taskpaper.replace(
+        /@tags\((.+)\)/gm,
+        `@tags($1, ${checklistTagName}, ${tagList})`
+      );
 
-    // add parent & checklist tags (where there aren't any exsting tags on line in taskpaper)
-    taskpaper = taskpaper.replace(
-      /(^((?!@tags).)*$)/gm,
-      `$1 @tags(${checklistTagName},${tagList})`
-    );
+      // add parent & checklist tags (where there aren't any exsting tags on line in taskpaper)
+      taskpaper = taskpaper.replace(
+        /(^((?!@tags).)*$)/gm,
+        `$1 @tags(${checklistTagName},${tagList})`
+      );
 
-    // replace '( )' with '[ ]'
-    taskpaper = taskpaper.replace(/\(\s\)/g, "[ ]");
+      // replace '( )' with '[ ]'
+      taskpaper = taskpaper.replace(/\(\s\)/g, "[ ]");
 
-    // replace '< >' with '( )'
-    taskpaper = taskpaper.replace(/\<\s\>/g, "( )");
+      // replace '< >' with '( )'
+      taskpaper = taskpaper.replace(/\<\s\>/g, "( )");
 
-    // build URL to paste tasks
-    var pasteUrlStr =
-      "omnifocus:///paste?target=/task/" +
-      encodeURIComponent(task.id.primaryKey) +
-      "&content=" +
-      encodeURIComponent(taskpaper);
+      // build URL to paste tasks
+      var pasteUrlStr =
+        "omnifocus:///paste?target=/task/" +
+        encodeURIComponent(task.id.primaryKey) +
+        "&content=" +
+        encodeURIComponent(taskpaper);
 
-    //open URL (generating subtasks)
-    URL.fromString(pasteUrlStr).call(() => {
-      // return to starting perspective
-      console.log(startingPerspective);
-      if (startingPerspective === Perspective.BuiltIn.Projects) {
-        taskId = task.id.primaryKey;
-        taskUrlStr = "omnifocus:///task/" + taskId;
-        URL.fromString(taskUrlStr).call(() => {});
-      } else if (startingPerspective instanceof Perspective.BuiltIn) {
-        document.windows[0].perspective = startingPerspective;
-      } else {
-        var perspectiveUrlStr =
-          "omnifocus:///perspective/" +
-          encodeURIComponent(startingPerspective.name);
-        URL.fromString(perspectiveUrlStr).call(() => {});
-      }
-    });
+      //open URL (generating subtasks)
+      URL.fromString(pasteUrlStr).call(() => {
+        // return to starting perspective
+        console.log(startingPerspective);
+        if (startingPerspective === Perspective.BuiltIn.Projects) {
+          taskId = task.id.primaryKey;
+          taskUrlStr = "omnifocus:///task/" + taskId;
+          URL.fromString(taskUrlStr).call(() => {});
+        } else if (startingPerspective instanceof Perspective.BuiltIn) {
+          document.windows[0].perspective = startingPerspective;
+        } else {
+          var perspectiveUrlStr =
+            "omnifocus:///perspective/" +
+            encodeURIComponent(startingPerspective.name);
+          URL.fromString(perspectiveUrlStr).call(() => {});
+        }
+      });
+    }
   };
 
   return functionLibrary;

17e659998363b85e2bb0a2741353ca521cbb80b9
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2021-12-02 17:04:43 +1100

    feat: add optional 'expandable' tag to indicate a task whose note can be expanded
    closes #6

--- NoteToSubtasks.omnifocusjs/Resources/preferences.js
+++ NoteToSubtasks.omnifocusjs/Resources/preferences.js
@@ -5,6 +5,7 @@
 
     // get current preferences or set defaults if they don't yet exist
     const checklistTag = this.noteToSubtasksLib.getChecklistTag()
+    const expandableTag = this.noteToSubtasksLib.getExpandableTag()
     const uninheritedTags = this.noteToSubtasksLib.getUninheritedTags()
     const tagsToRemove = this.noteToSubtasksLib.getTagsToRemove()
 
@@ -16,6 +17,11 @@
     checklistTagField.nullOptionTitle = 'None'
     form.addField(checklistTagField)
 
+    const expandableTagField = new Form.Field.Option('expandableTagID', 'Expandable Tag', flattenedTags, flattenedTags.map(t => t.name), expandableTag)
+    expandableTagField.allowsNull = true
+    expandableTagField.nullOptionTitle = 'None'
+    form.addField(expandableTagField)
+
     form.addField(new Form.Field.MultipleOptions('uninheritedTags', 'Uninherited Tags', flattenedTags, flattenedTags.map(t => t.name), uninheritedTags))
     form.addField(new Form.Field.MultipleOptions('tagsToRemove', 'Tags To Remove From Skipped Task When Expanding', flattenedTags, flattenedTags.map(t => t.name), tagsToRemove))
 
@@ -25,6 +31,7 @@
     if (form.values.checklistTagID !== null) preferences.write('checklistTagID', form.values.checklistTagID.id.primaryKey)
     preferences.write('uninheritedTagIDs', form.values.uninheritedTags.map(t => t.id.primaryKey))
     preferences.write('tagsToRemoveIDs', form.values.tagsToRemove.map(t => t.id.primaryKey))
+    if (form.values.expandableTagID !== null) preferences.write('expandableTagID', form.values.expandableTagID.id.primaryKey)
   })
 
   action.validate = function (selection, sender) {
